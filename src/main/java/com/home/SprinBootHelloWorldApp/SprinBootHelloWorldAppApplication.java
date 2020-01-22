0513df5e43c725a1513cd5326eaef83508488e3c
VivekGargHays <vivek.garg@hays.com>
2020-01-22 11:04:34 +0000

    code merge

---
+++ src/main/java/com/home/SprinBootHelloWorldApp/SprinBootHelloWorldAppApplication.java
@@ -0,0 +1,13 @@
+package com.home.SprinBootHelloWorldApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootHelloWorldAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootHelloWorldAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/SprinBootHelloWorldApp/SprinBootHelloWorldAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.SprinBootHelloWorldApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootHelloWorldAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
