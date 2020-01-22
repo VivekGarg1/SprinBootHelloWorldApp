568ad63abcb7d7b19bb6040f063527fca246dded
VivekGargHays <vivek.garg@hays.com>
2020-01-22 13:27:31 +0000

    code merge

--- src/main/java/com/home/SprinBootHelloWorldApp/GreetingController.java
+++ src/main/java/com/home/SprinBootHelloWorldApp/GreetingController.java
@@ -1,18 +1,21 @@
 package com.home.SprinBootHelloWorldApp;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4202")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/hello-world/api/v1/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
